198f254103f2cb755ce58122e5ed969c1a6d44d3
yusbel86 <yusbel1086@gmail.com>
2020-04-05 16:17:17 +0000

    prueba

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import Card from './component/card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Deployment in ZEIT for Hernan</h1>
+        <Card />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+function Card() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola desde un component</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Card;
